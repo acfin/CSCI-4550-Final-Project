ffad7c561f55b7f4513bfdc191e233700faf4c21
Cora <corradora30@gmail.com>
2023-04-30 09:40:42 -0400

    Resolution options, HUD scaling

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public GameObject toggle;
+    public TMP_Dropdown resolutionDropdown; // Reference to your Dropdown component
     public int sceneNum = 0;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // Add a listener to the dropdown
+        resolutionDropdown.onValueChanged.AddListener(delegate {
+            SetResolution(resolutionDropdown.value);
+        });
     }
 
     public void ReturnToMainMenu()
@@ -27,21 +22,37 @@ public class SettingsMenu : MonoBehaviour
         SceneManager.LoadScene(sceneNum);
     }
 
-    public void setScreenMode()
+    public void SetResolution(int index)
     {
-        if (Screen.fullScreenMode != FullScreenMode.Windowed)
+        switch (index)
         {
-            toggle.GetComponent<Toggle>().isOn = true;
-            //Screen.fullScreenMode = FullScreenMode.Windowed;
-            Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
-            
-        }
-
-        else
-        {
-            toggle.GetComponent<Toggle>().isOn = false;
-            Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow);
-            //Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+            case 0:
+                Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow);
+                break;
+            case 1:
+                Screen.SetResolution(1600, 900, FullScreenMode.FullScreenWindow);
+                break;
+            case 2:
+                Screen.SetResolution(1366, 768, FullScreenMode.FullScreenWindow);
+                break;
+            case 3:
+                Screen.SetResolution(1280, 720, FullScreenMode.FullScreenWindow);
+                break;
+            case 4:
+                Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+                break;
+            case 5:
+                Screen.SetResolution(1600, 900, FullScreenMode.Windowed);
+                break;
+            case 6:
+                Screen.SetResolution(1366, 768, FullScreenMode.Windowed);
+                break;
+            case 7:
+                Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+                break;
+            default:
+                Debug.LogError("Invalid resolution index");
+                break;
         }
     }
 }
