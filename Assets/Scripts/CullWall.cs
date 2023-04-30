df73f05ea970a9467c2e3ff26a48a56a2c593c0f
Cora <corradora30@gmail.com>
2023-04-30 17:23:19 -0400

    CullWall adjustment (modifies alpha)

--- Assets/Scripts/CullWall.cs
+++ Assets/Scripts/CullWall.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CullWall : MonoBehaviour
 {
     [SerializeField] MeshRenderer[] wallMeshes;
+    private float fadedAlpha = 0.2f; // Set the desired alpha value for the faded effect
     BoxCollider floor;
     bool isCulled = false;
+
     void Start()
     {
         floor = GetComponent<BoxCollider>();
@@ -14,9 +16,9 @@ public class CullWall : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.gameObject.name == "Player")
+        if (collision.collider.gameObject.name == "Player")
         {
-            CullWalls();
+            SetWallTransparency(fadedAlpha, true);
         }
     }
 
@@ -24,23 +26,34 @@ public class CullWall : MonoBehaviour
     {
         if (collision.collider.gameObject.name == "Player")
         {
-            UncullWalls();
+            SetWallTransparency(1f, false);
         }
     }
-
-    void CullWalls()
-    {
-        foreach(MeshRenderer wall in wallMeshes)
-        {
-            wall.forceRenderingOff = true;
-        }
-    }
-
-    void UncullWalls()
+    
+    // Changes the Wall transparency when the player is in front of it, sets back to alpha when not in front.
+    void SetWallTransparency(float alpha, bool transparent)
     {
         foreach (MeshRenderer wall in wallMeshes)
         {
-            wall.forceRenderingOff = false;
+            Color baseColor = wall.material.GetColor("_BaseColor");
+            baseColor.a = alpha;
+            wall.material.SetColor("_BaseColor", baseColor);
+
+            if (transparent)
+            {
+                wall.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                wall.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                wall.material.SetInt("_ZWrite", 0);
+                wall.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                wall.material.renderQueue = 3000;
+            }
+            else
+            {
+                wall.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                wall.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                wall.material.SetInt("_ZWrite", 1);
+                wall.material.renderQueue = -1;
+            }
         }
     }
 }
